97a458c1fc5d31586d4f70314171ebab6aae2c89
Ismail Ismailov <ismail2ov@gmail.com>
2023-01-26 23:56:22 +0100

    Add contracts to the producer project

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# spring-cloud-contract-angular-consumer
-Angular example with Docker Spring Cloud Contract Stub Runner
+# Angular example with Docker Spring Cloud Contract Stub Runner
+
+This is a simple example of how to use Spring Cloud Contract to create a contract between a Spring application (_which is the **Producer**_) and an Angular application (_which is the **Consumer**_).
+
+## Producer
+
+We have created a simple application with Spring Boot that does not yet have an implementation, but we have the contracts to be able to create the contracts.
+
+### Create the stubs
+
+- Go to the catalog folder  
+``` shell
+ cd catalog
+```
+
+- Compile and package the code skipping the tests because we don't have any logic implemented yet  
+``` shell
+ mvn clean package -DskipTests
+```
+
---
+++ catalog/src/test/java/com/github/ismail2ov/catalog/BaseTestClass.java
@@ -0,0 +1,25 @@
+package com.github.ismail2ov.catalog;
+
+import com.github.ismail2ov.catalog.infrastructure.controller.ProductController;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+public class BaseTestClass {
+
+    @Autowired
+    private ProductController productController;
+
+    @BeforeEach
+    public void setup() {
+        StandaloneMockMvcBuilder standaloneMockMvcBuilder = MockMvcBuilders.standaloneSetup(productController);
+        RestAssuredMockMvc.standaloneSetup(standaloneMockMvcBuilder);
+    }
+}
